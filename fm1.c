6b97062b6b9078626bf09e2737ec9c77cda95315
ManikanthReddyMandala <manimandala99@gmail.com>
2018-10-13 19:54:18 +0530

    fm1 program

---
+++ fm1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i,ct,n=1,j;
+   for(i=1;i<=4;i++)
+   {
+       ct=0;
+       for(j=i;j<=3;j++)
+       {
+           printf("\t");
+       }
+       while(ct!=i)
+      {
+          ct++;
+
+          printf("\t\t%d",n);
+          n++;
+      }
+       printf("\n");
+       printf("\n");
+   }
+}
